2ed81e55e455573141fe494eaa20cac04b77189f
BRICK\mikus <nulsenbrad@gmail.com>
2024-09-15 08:57:34 -0400

    aurora borealis multiplayer node

--- Scenes/borealis/WorldEnvironment.gd
+++ Scenes/borealis/WorldEnvironment.gd
@@ -21,22 +21,33 @@ const ambient_blue_zsf = Vector3(0.5, 0.25, 8)
 const ambient_energy_zsf = Vector3(0.5, 0.25, 1)
 
 func _process(delta):
+	
+	if not is_multiplayer_authority():
+		
+		return
+		
 	timer += delta
+	set_environment_phase.rpc(timer)
 	
-	environment.fog_light_energy = get_zsf_instant(timer, fog_light_energy_zsf)
-	environment.fog_density = get_zsf_instant(timer, fog_density_zsf)
-	environment.fog_height = get_zsf_instant(timer, fog_height_zsf) + get_zsf_instant(timer, fog_height_wiggle_zsf)
-	environment.fog_height_density = get_zsf_instant(timer, fog_height_density_zsf)
 	
-	var ambient_red = get_zsf_instant(timer, ambient_red_zsf)	
-	var ambient_green = get_zsf_instant(timer, ambient_green_zsf)	
-	var ambient_blue = get_zsf_instant(timer, ambient_blue_zsf)	
+	
+	
+func get_zsf_instant(phase, zsf, offset = 0):
+	return zsf.x + zsf.y * sin(offset + phase/zsf.z) 
+
+@rpc("call_local")
+func set_environment_phase(phase):
+	environment.fog_light_energy = get_zsf_instant(phase, fog_light_energy_zsf)
+	environment.fog_density = get_zsf_instant(phase, fog_density_zsf)
+	environment.fog_height = get_zsf_instant(phase, fog_height_zsf) + get_zsf_instant(phase, fog_height_wiggle_zsf)
+	environment.fog_height_density = get_zsf_instant(phase, fog_height_density_zsf)
+	
+	var ambient_red = get_zsf_instant(phase, ambient_red_zsf)	
+	var ambient_green = get_zsf_instant(phase, ambient_green_zsf)	
+	var ambient_blue = get_zsf_instant(phase, ambient_blue_zsf)	
 
 	environment.ambient_light_color = Color(ambient_red, ambient_green, ambient_blue)
 	#environment.volumetric_fog_emission_energy = get_zsf_instant(timer, fog_density_zsf)
 	
 	environment.ambient_light_energy = get_zsf_instant(timer, ambient_energy_zsf)
 	
-	
-func get_zsf_instant(phase, zsf, offset = 0):
-	return zsf.x + zsf.y * sin(offset + phase/zsf.z) 
