c53fd63a1422baf95e9976081950ed3516313d80
Brad Nulsen <brad.nulsen@grantek.com>
2024-10-17 23:34:40 -0400

    wig color variants

--- Scenes/objects/wig/Wig.gd
+++ Scenes/objects/wig/Wig.gd
@@ -1,5 +1,18 @@
 extends RigidBody3D
 
+@export var HAIR_COLOR : Color:
+	
+	get:
+		return HAIR_COLOR
+		
+	set(value):
+		HAIR_COLOR = value
+		var model = $MeshInstance3D
+		var material = model.get_surface_override_material(0)
+		material.albedo_color = value
+		model.set_surface_override_material(0, material)
+
+
 @export var radius = 0.25
 
 @onready var mesh = $MeshInstance3D
@@ -16,6 +29,13 @@ func _enter_tree():
 	add_to_group("wigs")
 	contact_monitor = true
 	max_contacts_reported = 10
+	
+func _ready():
+	
+	if not is_multiplayer_authority(): 
+		return
+		
+	getRandomHairColor()
 
 
 func _process(_delta):
@@ -25,7 +45,13 @@ func _process(_delta):
 	collider.shape.radius = radius
 	
 
+func getRandomHairColor():
 	
+	var rng = RandomNumberGenerator.new()
+	var colorBase = rng.randf_range(0.0, 200.0 ) / 255
+	var redShift = rng.randf_range(0.0, colorBase / 3.0) / 255
+	var greenShift = rng.randf_range(0.0, redShift ) / 255
+	HAIR_COLOR = Color(colorBase + redShift, colorBase + greenShift, colorBase)
 
 
 	
