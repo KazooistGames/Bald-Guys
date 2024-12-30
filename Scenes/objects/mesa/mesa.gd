d20b6ea4acc92be033736cc822d48ad3944ecd3e
Brad Nulsen <brad.nulsen@grantek.com>
2024-12-29 22:20:19 -0500

    map design!!!!!

--- Scenes/objects/mesa/mesa.gd
+++ Scenes/objects/mesa/mesa.gd
@@ -1,10 +1,16 @@
 extends Node3D
 
 
+enum Preference{
+	shallow = 0,
+	deep = 1
+}
+
+@export var preference = Preference.deep
+
 @onready var mesh = $MeshInstance3D
 @onready var collider = $CollisionShape3D
 @onready var raycast = $RayCast3D
-
 
 @export var top_height = 0.0
 @export var bottom_drop = 0.0
@@ -14,28 +20,21 @@ var bottom_position = Vector3.ZERO
 
 
 func _process(_delta):
-		
+	
 	if not raycast.is_colliding():
 		pass
 		
 	elif raycast.get_collision_point() != bottom_position:
 		
-		bottom_position = raycast.get_collision_point() - global_position
-		var top_position = get_top_position(bottom_position)
+		var new_point = raycast.get_collision_point()
 		
-		var mesh_position = get_mesh_position(bottom_position)
-		var mesh_height = get_mesh_height(bottom_position)
-			
-		mesh.global_position = mesh_position + global_position
-		mesh.mesh.size.x = radius
-		mesh.mesh.size.y = radius
-		mesh.mesh.size.z = mesh_height 
+		if just_deeper(new_point) and preference == Preference.shallow:
+			return
+		elif just_shallower(new_point) and preference == Preference.deep:
+			return
 		
-		collider.global_position = mesh_position + global_position
-		collider.shape.size.y = mesh_height
-		collider.shape.size.x = radius
-		collider.shape.size.z = radius
-		
+		rerender()
+
 
 func get_top_position(bot_pos):
 	
@@ -54,3 +53,46 @@ func get_mesh_height(bot_pos):
 	return top_position.distance_to(bot_pos) + bottom_drop
 	
 	
+func rerender():
+
+	bottom_position = raycast.get_collision_point() - global_position
+	var top_position = get_top_position(bottom_position)
+	
+	var mesh_position = get_mesh_position(bottom_position)
+	var mesh_height = get_mesh_height(bottom_position)
+		
+	mesh.global_position = mesh_position + global_position
+	mesh.mesh.size.x = radius
+	mesh.mesh.size.y = radius
+	mesh.mesh.size.z = mesh_height 
+	
+	collider.global_position = mesh_position + global_position
+	collider.shape.size.y = mesh_height
+	collider.shape.size.x = radius
+	collider.shape.size.z = radius
+		
+		
+func just_deeper(new_point):
+	
+	var new_trajectory = (new_point - global_position)
+	
+	if bottom_position.normalized() != new_trajectory.normalized():
+		return false
+	elif new_trajectory.length() > bottom_position.length():
+		return true
+	else:
+		return false
+		
+
+func just_shallower(new_point):
+	
+	var new_trajectory = (new_point - global_position)
+	
+	if bottom_position.normalized() != new_trajectory.normalized():
+		return false
+		
+	elif new_trajectory.length() < bottom_position.length():
+		return true
+		
+	else:
+		return false
