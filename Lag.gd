c1fb933113f35d9bf7399ce867be56dd14eb2b9b
BRICK\mikus <nulsenbrad@gmail.com>
2025-09-20 18:02:07 -0400

    what the fuck

--- Lag.gd
+++ Lag.gd
@@ -13,11 +13,10 @@ func _physics_process(delta) -> void:
 		
 	var local_server_time = Time.get_ticks_msec()
 		
-	if is_multiplayer_authority():
+	if not multiplayer.has_multiplayer_peer():
+		pass
+	elif is_multiplayer_authority():
 		send_timestamp.rpc(local_server_time)
-	else:
-		#print("sending ", local_server_time)
-		send_timestamp.rpc_id(1, local_server_time)
 				
 	
 @rpc("any_peer", "call_remote")
@@ -26,19 +25,20 @@ func send_timestamp(passthrough_time : float):
 	var sender_id = multiplayer.get_remote_sender_id()
 	return_timestamp.rpc_id(sender_id, passthrough_time) #return fire to original send_timestamper
 	
-	if not is_multiplayer_authority():
+	if not is_multiplayer_authority(): #clients should initiate their own ping-pong for server lag.
 		var local_client_time = Time.get_ticks_msec()
+		#print('sending ', local_client_time)
 		send_timestamp.rpc_id(get_multiplayer_authority(), local_client_time)
 		
 		
 @rpc("any_peer", "call_remote")
-func return_timestamp(original_timestamp : float): #responding RPC call that passes back initial timestamp to original send_timestamper
+func return_timestamp(original_timestamp : float):
 	
 	var local_timestamp = Time.get_ticks_msec()
-	var RTT = (local_timestamp - original_timestamp) * 1000.0
+	var RTT = (local_timestamp - original_timestamp) / 1000.0
 	
 	if is_multiplayer_authority():
 		CLIENT_PINGS[multiplayer.get_remote_sender_id()] = RTT / 2.0	
 	else:
-		#print("receiving ", send_timestamp_timestamp)
+		#print("receiving ", local_timestamp)
 		SERVER_PING = RTT / 2.0
